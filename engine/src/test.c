ba55592e02c92051e2b33c8e9493d5ac3c02e99a
Oscar Guzman <oscar.guzman9501@gmail.com>
2025-04-27 23:07:12 -0500

    first configuration for game engine

---
+++ engine/src/test.c
@@ -0,0 +1,6 @@
+#include "test.h"
+#include <stdio.h>
+
+void print_int(i32 i) {
+    printf("el numero es  %i", i);
+}
---
+++ testbed/build.bat
@@ -0,0 +1,26 @@
+REM Build script for testbed
+@ECHO OFF
+SetLocal EnableDelayedExpansion
+
+REM Get a list of all the .c files.
+SET cFilenames=
+FOR /R src %%f in (*.c) do (
+    SET cFilenames=!cFilenames! %%f
+)
+
+ECHO "Files to compile:" %cFilenames%
+
+SET assembly=testbed
+SET compilerFlags=-g
+REM -Wall -Werror -v
+SET includeFlags=-Isrc -I../engine/src
+SET linkerFlags=-L../bin/ -lengine.lib
+SET defines=-D_DEBUG -DKIMPORT
+
+ECHO "Building %assembly%..."
+ECHO "Command: C:\Program Files\LLVM\bin\clang.exe" %cFilenames% %compilerFlags% -o ../bin/%assembly%.exe %defines% %includeFlags% %linkerFlags%
+
+"C:\Program Files\LLVM\bin\clang.exe" %cFilenames% %compilerFlags% -o ../bin/%assembly%.exe %defines% %includeFlags% %linkerFlags%
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO "Error al compilar el testbed. Código: %ERRORLEVEL%"
+)
